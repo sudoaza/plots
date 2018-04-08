66a27cbd578efe280f07ab17e5e4cd295600facc
aza <aza@lainventoria.com.ar>
2018-04-08 19:48:04 +0200

    kmeans clustering and plot it

--- hist.py
+++ hist.py
@@ -2,8 +2,17 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('rand.csv')
 
+# Arguments
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("file", type=file, help="Data CSV file")
+args = parser.parse_args()
+
+# Open
+df = pd.read_csv(args.file)
+
+# Plot
 n, bins, patches = plt.hist(df['random'], 100, density=True, facecolor='g', alpha=0.75)
 
 plt.show()
