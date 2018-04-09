a6e15433ac3b40ae5ebbeec0fb42b24cc27553fe
Rodrigo <rodrigo@wundertax.de>
2018-04-09 16:10:17 +0200

    fix para histogram

--- hist.py
+++ hist.py
@@ -6,13 +6,13 @@ import matplotlib.pyplot as plt
 # Arguments
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("file", type=file, help="Data CSV file")
+parser.add_argument("file", help="Data CSV file")
 args = parser.parse_args()
 
 # Open
-df = pd.read_csv(args.file)
+df = pd.read_csv(args.file).as_matrix()
 
 # Plot
-n, bins, patches = plt.hist(df['random'], 100, density=True, facecolor='g', alpha=0.75)
+n, bins, patches = plt.hist(df[:,0], 100, density=True, facecolor='g', alpha=0.75)
 
 plt.show()
