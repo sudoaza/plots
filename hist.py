6f427cb23fbee7f0290ea3316c7a83c41c18d4fd
Rodrigo <rodrigo@wundertax.de>
2018-04-10 11:34:48 +0200

    more options for histogram script

--- hist.py
+++ hist.py
@@ -7,12 +7,26 @@ import matplotlib.pyplot as plt
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="Data CSV file")
+parser.add_argument("-c","--column", type=int, help="Column of data to plot", default=0)
+parser.add_argument("-b","--buckets", help="Number of buckets", default=100)
+parser.add_argument("-d","--deviations", type=float, help="Keep this many times the standard deviation, used for cliping the plot.", default=3)
 args = parser.parse_args()
 
 # Open
-df = pd.read_csv(args.file).as_matrix()
+data = pd.read_csv(args.file).as_matrix()
+
+# Pick the column we are ploting
+data = data[:,args.column]
+
+mean = np.mean(data)
+sd = np.std(data)
+
+clip_min = mean - args.deviations * sd
+clip_max = mean + args.deviations * sd
+
+data = np.clip(data, clip_min, clip_max)
 
 # Plot
-n, bins, patches = plt.hist(df[:,0], 100, density=True, facecolor='g', alpha=0.75)
+n, bins, patches = plt.hist(data, args.buckets, density=True, facecolor='g', alpha=0.75)
 
 plt.show()
